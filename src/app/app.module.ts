ac89d251ad27e9c213d8bcdfd49489701da8408d
mario32111 <marioge44@gmail.com>
2025-02-19 09:46:17 -0600

    footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent // Asegúrate de agregar el componente Header aquí
+    HeaderComponent, // Asegúrate de agregar el componente Header aquí
+    FooterComponent,
   ],
   imports: [
     CommonModule,
